1e8d7fccf601f94330d86043d140a3165d6292c5
serhanelmali <serhanelmali@gmail.com>
2021-10-07 14:37:14 +0300

    feat(Services): adds services component

--- components/services/index.js
+++ components/services/index.js
@@ -1,4 +1,5 @@
 import useSWR from "swr";
+import styles from "./services.module.scss";
 
 const fetcher = async () => {
   const response = await fetch(
@@ -8,15 +9,32 @@ const fetcher = async () => {
   return data;
 };
 
-const ServicesSWR = () => {
+const Services = () => {
   const { data, error } = useSWR("services", fetcher);
 
   if (error) return "An error has occured";
   if (!data) return "Loading";
 
   return data.map((service, id) => {
-    return <p key={id}>{service.title}</p>;
+    return (
+      <div className={styles.service} key={id}>
+        <img className={styles.service__icon} src={service.icon} />
+        <div className={styles.service__category}>
+          <span>{service.title}</span>
+        </div>
+        <div className={styles.service__experts}>
+          <span className={styles.service__experts__title}>Experts</span>
+          <div className={styles.service__experts__images}>
+            <img src={service.people[0].avatar} />
+            <img src={service.people[1].avatar} />
+          </div>
+          <span className={styles.service__experts__names}>
+            {service.people[0].name} + {service.people[1].name}
+          </span>
+        </div>
+      </div>
+    );
   });
 };
 
-export default ServicesSWR;
+export default Services;
