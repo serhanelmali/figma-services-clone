3c091d049ced441cef417a526d1c430c8bae3f45
serhanelmali <serhanelmali@gmail.com>
2021-10-09 23:30:02 +0300

    feat(services): adds infinite scroll

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import styles from "./navbar.module.scss";
 
@@ -23,16 +23,25 @@ const Navbar = () => {
         <div></div>
         <div></div>
       </div>
-      <ul
+      <div
         className={
           !isHamburger ? styles.navbar__pages : styles.navbar__pages__active
         }
       >
-        <li>How It Works</li>
-        <li>Advantages</li>
-        <li>Services</li>
-        <li>Apply to be an expert</li>
-      </ul>
+        <ul>
+          <li>How It Works</li>
+          {!isHamburger ? <li>Advantages</li> : <li> Advantages of Great</li>}
+          {!isHamburger ? <li>Services</li> : <li>45 Expert/Categories</li>}
+          <li>Apply to be an expert</li>
+
+          <li>
+            <Image width={30} height={30} src="/images/YoutubeIcon.svg" />
+            <Image width={30} height={30} src="/images/FacebookIcon.svg" />
+            <Image width={30} height={30} src="/images/InstagramIcon.svg" />
+            <Image width={30} height={30} src="/images/LinkedInIcon.svg" />
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
